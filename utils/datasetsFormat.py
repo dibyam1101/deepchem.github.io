df8c3f0b0b8457951f45f77559f8e825cdb3c628
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2022-12-15 00:22:54 +0530

    Python script to extract row data from every dataset

---
+++ utils/datasetsFormat.py
@@ -0,0 +1,36 @@
+import os
+import gzip
+import shutil
+fromPath = "deepchem/data/datasets/"
+toPath = "deepchem/data/datasetsFormat/"
+
+fileNames = os.listdir(fromPath)
+
+for fileName in fileNames:
+    if fileName.endswith(".csv"):
+        shutil.copyfile(fromPath + fileName, toPath + fileName)
+    elif fileName.endswith(".pkl.gz"):
+        continue
+    elif fileName.endswith(".gz"):
+        inFile = fromPath + fileName
+        outfile = toPath + fileName[:-3]
+        with gzip.open(inFile, 'rb') as f_in:
+            with open (outfile, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+    else:
+        continue
+
+# load csv files
+import pandas as pd
+import numpy as np
+
+fileNames = os.listdir(toPath)
+for fileName in fileNames:
+    file = toPath + fileName
+    df = pd.read_csv(file)
+
+    # convert the first 5 rows to json
+    reqd = df.iloc[0:5].to_json(orient='records')
+    # save the json to a file
+    with open(file[:-3] + 'json', 'w') as outfile:
+        outfile.write(reqd)
